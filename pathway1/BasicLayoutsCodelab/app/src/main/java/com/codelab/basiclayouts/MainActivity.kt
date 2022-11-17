b724cefa8521479efce1511feaacf6684c740d98
DavidKwon7 <khjunejune10@gmail.com>
2022-11-18 00:17:24 +0900

    pathway1.2: BasicLayoutsCodelab

--- pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ pathway1/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -32,103 +32,236 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp(modifier = Modifier.fillMaxSize())
+            MySootheApp(modifier = Modifier.fillMaxSize())
         }
     }
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by rememberSaveable {
-        mutableStateOf(true)
-    }
-
-    Surface(modifier = modifier) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
-        } else {
-            Greetings()
+fun MySootheApp() {
+    MyApplication1123Theme {
+        Scaffold(
+            bottomBar = { SootheBottomNavigation()}
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
         }
     }
 }
 
 @Composable
-fun OnboardingScreen(
-    onContinueClicked: () -> Unit,
+private fun SootheBottomNavigation(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = modifier
+    ) {
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Home, contentDescription = null)
+            },
+            label = {
+                Text(text = stringResource(id = R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {}
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Home, contentDescription = null)
+            },
+            label = {
+                Text(text = stringResource(id = R.string.bottom_navigation_profile))
+            },
+            selected = false, onClick = { })
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
+    ) {
+        //Spacer(modifier = Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        //Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(id = title).uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    // LazyHorizontalGrid은 compose 특정 버전 이상에서부터 사용 가능!
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(
+                drawable = item.drawable,
+                text = item.text,
+                modifier = Modifier.heightIn(56.dp)
+            )
+        }
+    }
+}
+
+// Surface는 앱의 테마를 변경하여 조절할 수 있다는 특징이 있습니다
+@Composable
+fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(
+                drawable = item.drawable,
+                text = item.text
+            )
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
     ) {
-        Text("Welcome to the Basics Codelab!")
-        Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
-        ) {
-            Text("Continue")
-        }
-    }
-}
-
-@Composable
-fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(1000) {"$it"}
-) {
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-
-    // Animation 추가
-    val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
+        Image(
+            // contentDescription 으로 추가 설명 가능
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
         )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Hello,")
-                Text(text = name, style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                ))
-            }
-            Button(
-                onClick = { expanded.value = !expanded.value },
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
-        }
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            )
+        )
     }
 }
 
-
-
-@Preview(showBackground = true)
 @Composable
-fun MyAppPreview() {
-    MyApplication1123Theme {
-        MyApp(Modifier.fillMaxSize())
-    }
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
 }
+
+// 데이터
+private val alignYourBodyData = listOf(
+    R.drawable.ic_launcher_background to R.string.ab1_inversions,
+    R.drawable.ic_launcher_background to R.string.ab1_inversions,
+    R.drawable.ic_launcher_background to R.string.ab1_inversions,
+    R.drawable.ic_launcher_background to R.string.ab1_inversions,
+    R.drawable.ic_launcher_background to R.string.ab1_inversions,
+    R.drawable.ic_launcher_background to R.string.ab1_inversions
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations,
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations,
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations,
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations,
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations,
+    R.drawable.ic_launcher_foreground to R.string.fc2_nature_meditations
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
 
 @Preview(
     showBackground = true,
@@ -140,7 +273,7 @@ fun MyAppPreview() {
 @Composable
 fun DarkModePreview() {
     MyApplication1123Theme {
-        Greetings()
+        SearchBar()
     }
 }
 
@@ -148,6 +281,16 @@ fun DarkModePreview() {
 @Composable
 fun MyAppPreview() {
     MyApplication1123Theme {
-        MyApp(Modifier.fillMaxSize())
+        HomeSection(title = R.string.align_your_body) {
+            SootheBottomNavigation()
+        }
     }
+}
+
+@Preview(showBackground = true,
+    backgroundColor = 0xFFF0EAE2,
+    heightDp = 180)
+@Composable
+fun ScreenContentPreview() {
+    MyApplication1123Theme { HomeScreen() }
 }
