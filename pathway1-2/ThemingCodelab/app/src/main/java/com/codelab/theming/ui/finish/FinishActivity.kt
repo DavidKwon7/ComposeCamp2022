7cfb49929db4fbb9272de9237238f014eddc744c
DavidKwon7 <khjunejune10@gmail.com>
2022-11-21 16:06:49 +0900

    pathway1-2.2: ThemingCodelab

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/finish/FinishActivity.kt
@@ -0,0 +1,10 @@
+class FinishActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface {
+                Home()
+            }
+        }
+    }
+}
